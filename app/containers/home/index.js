02091d6b2c1c82c2b8e4697b672ad94a3942f2bf
kyangy <yangdongkevin@gmail.com>
2016-12-29 13:29:21 -0800

    Added assets and set up action calls

--- app/containers/home/index.js
+++ app/containers/home/index.js
@@ -1,5 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+
+import { signUp } from 'actions/users'
+
 import classNames from 'classnames/bind';
 import styles from './index.css'
 
@@ -7,10 +11,45 @@ const cx = classNames.bind(styles);
 
 class Home extends Component {
 
+  state = {
+    email: ''
+  }
+
+  onChangeEmail = (email) => {
+    this.setState({ email: email })
+  }
+
+  _signUp = () => {
+    const { signUp } = this.props
+    const { email } = this.state
+
+    console.log(email)
+
+    if (email !== '') signUp(email)
+  }
+
   render() {
     return (
       <div className='container-fluid'>
-        <p className='text-center'>KYANGY</p>
+        <div className='row'>
+          <div className='col-md-6'>
+            <img className={ cx('logo') } src='imgs/logo.png' />
+          </div>
+          <div className='col-md-6'>
+            <h1>Your chocolate cravings delivered to your door!</h1>
+            <input placeholder='Email' onChange={ (email) => this.onChangeEmail(email) } />
+            <button onClick={ this._signUp }>Sign Up</button>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-md-6'>
+            <img className={ cx('logo') } src='/imgs/oreo.png' />
+          </div>
+          <div className='col-md-6'>
+            <img className={ cx('logo') } src='/imgs/milkman.png' />
+          </div>
+        </div>
       </div>
     );
   }
@@ -21,4 +60,10 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    signUp
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
